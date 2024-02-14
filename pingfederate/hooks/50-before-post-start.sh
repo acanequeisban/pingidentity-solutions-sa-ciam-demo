a40dab2f4192704a87245b2c0746f4ad335bb7d5
Alejandro SDS <33510391+acanequeisban@users.noreply.github.com>
2024-02-14 09:52:38 +0000

    Correct LDAP Ports

--- pingfederate/hooks/50-before-post-start.sh
+++ pingfederate/hooks/50-before-post-start.sh
@@ -17,12 +17,22 @@ echo Hello from the server profile 50-before-post-start.sh hook!
 # - AuthN Sessions
 # These are mapped later in the /config-store API calls
 
+echo ## initial 50-before-post-start.sh -> original hivemodule.xml ##
+cat /opt/out/instance/server/default/conf/META-INF/hivemodule.xml
+echo ####################################################
+
 sed -e "s#<construct class=\"org.sourceid.oauth20.domain.ClientManagerXmlFileImpl\"/>#<construct class=\"org.sourceid.oauth20.domain.ClientManagerLdapImpl\"/>#" \
     -e "s#<create-instance class=\"org.sourceid.oauth20.token.AccessGrantManagerJdbcImpl\"/>#<create-instance class=\"org.sourceid.oauth20.token.AccessGrantManagerLDAPPingDirectoryImpl\"/>#" \
     -e "s#<construct class=\"org.sourceid.saml20.service.session.data.impl.SessionStorageManagerJdbcImpl\"/>#<construct class=\"org.sourceid.saml20.service.session.data.impl.SessionStorageManagerLdapImpl\"/>#"\
     "/opt/out/instance/server/default/conf/META-INF/hivemodule.xml" > "/opt/out/instance/server/default/conf/META-INF/hivemodule.xml-modified"
 
+echo ## post sed 50-before-post-start.sh -> create hivemodule-modified.xml ##
+cat /opt/out/instance/server/default/conf/META-INF/hivemodule.xml-modified
+echo ####################################################
+
+echo ## rename sed 50-before-post-start.sh -> original hivemodule.xml to hivemodule.xml-modified ##
 mv /opt/out/instance/server/default/conf/META-INF/hivemodule.xml-modified /opt/out/instance/server/default/conf/META-INF/hivemodule.xml
+echo ####################################################
 
 # Delete bundled files so that Server Profile can apply newer ones
 echo Removing bundled files
